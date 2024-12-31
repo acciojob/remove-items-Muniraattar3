babbb7283c8c6131086bcf5a28b6b480a2d40e85
Munira Yusuf Ali Attar <124035031+Muniraattar3@users.noreply.github.com>
2024-12-31 14:39:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.querySelector('input[typr="button"]').addEventListener("click", function(){
+	const colorSelect = document.getElementById("colorSelect");
+
+	const selectedOption = colorSelect.options[colorSelect.selectedIndex];
+
+	if(selectedOption){
+		colorSelect.remove(colorSelect.selectedIndex);
+	}
+});
